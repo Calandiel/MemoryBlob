091325778d340c77d01f32c228ee3718529490cd
Calandiel <Calandiel@users.noreply.github.com>
2022-06-29 15:17:08 +0200

    Updated reduce and setstate on MemoryBlob

--- memblob.py
+++ memblob.py
@@ -1,22 +1,26 @@
 from ctypes import *
 
 class MemoryBlob:
-	def __init__(self, size: int = 0, array_type = c_byte):
-		self._data = (array_type * size)()
+	def __init__(self, size: int = 0, array_type = c_ubyte):
 		self._array_type = array_type
+		self._data = (array_type * size)()
+
+	def binary_length(self):
+		return len(self._data) * sizeof(self._array_type)
 
 	def __reduce__(self) -> list:
 		state = []
 		state.append(self._array_type)
 		state.append(len(self._data))
-		for d in self._data:
-			state.append(d)
+		state_bytes = bytearray(self._data)
+		state.append(state_bytes)
 		return (self.__class__, (), state)
 
 	def __setstate__(self, state: list):
-		self._data = (state[1] * state[0])()
-		for i in range(0, len(self._data)):
-			self._data[i] = state[2 + i]
+		self._array_type = state[0]
+		data_type = state[0] * state[1]
+		input_binary = state[2]
+		self._data = data_type.from_buffer(input_binary)
 
 	def __len__(self) -> int:
 		return len(self._data)
